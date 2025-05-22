b1c6281889fdebba5c528183d1caac65627ec595
Hari Om <hariom1gyan@gmail.com>
2025-05-22 10:22:13 +0530

    database issue

--- app/Helpers/DatabaseHelper.php
+++ app/Helpers/DatabaseHelper.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
+use App\Models\Agency;
 
 class DatabaseHelper
 {
