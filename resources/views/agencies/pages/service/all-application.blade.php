16fe4b82bbbae6a6dc58e5a87ce1526d29c2766e
Hari Om <hariom1gyan@gmail.com>
2025-11-03 10:04:36 +0530

    All Data is Done

--- resources/views/agencies/pages/service/all-application.blade.php
+++ resources/views/agencies/pages/service/all-application.blade.php
@@ -25,7 +25,7 @@
     
             <div class="w-full overflow-x-auto p-4">
             
-                <table class="w-full border-[2px] border-secondary/40 border-collapse mt-4">
+              {{--   <table class="w-full border-[2px] border-secondary/40 border-collapse mt-4">
                     <tr>
                         <td class="border-[2px] border-secondary/40 bg-gray-100 px-4 py-1.5 text-ternary/80 font-bold text-md">Sr. No.</td>
                         <td class="border-[2px] border-secondary/40 bg-gray-100 px-4 py-1.5 text-ternary/80 font-bold text-md">Service Name</td>
@@ -113,7 +113,95 @@
                     @endforelse
 
 
-                </table>
+                </table>--}}
+                <table class="w-full border-[2px] border-secondary/40 border-collapse mt-4">
+    <tr>
+        <td class="border-[2px] border-secondary/40 bg-gray-100 px-4 py-1.5 font-bold text-md">Sr. No.</td>
+        <td class="border-[2px] border-secondary/40 bg-gray-100 px-4 py-1.5 font-bold text-md">Service Name</td>
+        <td class="border-[2px] border-secondary/40 bg-gray-100 px-4 py-1.5 font-bold text-md">Country</td>
+        <td class="border-[2px] border-secondary/40 bg-gray-100 px-4 py-1.5 font-bold text-md">Visa Details</td>
+        <td class="border-[2px] border-secondary/40 bg-gray-100 px-4 py-1.5 font-bold text-md">Nationality</td>
+        <td class="border-[2px] border-secondary/40 bg-gray-100 px-4 py-1.5 font-bold text-md">Client Information</td>
+        <td class="border-[2px] border-secondary/40 bg-gray-100 px-4 py-1.5 font-bold text-md">Date of Entry</td>
+        <td class="border-[2px] border-secondary/40 bg-gray-100 px-4 py-1.5 font-bold text-md">Action</td>
+    </tr>
+
+    @forelse($requestDatas as $requestData)
+        <tr class="{{ $loop->iteration % 2 === 0 ? 'bg-gray-100/40' : '' }} hover:bg-secondary/10 cursor-pointer transition ease-in duration-2000">
+            <td class="border-[2px] border-secondary/40 px-4 py-1">{{ $loop->iteration }}</td>
+
+            <td class="border-[2px] border-secondary/40 px-4 py-1">
+                {{ $requestData->service_type ?? 'N/A' }}
+            </td>
+
+            {{-- Country Section --}}
+            <td class="border-[2px] border-secondary/40 px-4 py-0.5 text-sm">
+                <span class="flex items-center gap-2 mr-6 whitespace-nowrap">
+                    <img src="{{ asset('assets/flags/64x48/' . strtolower($requestData->combination?->origincountry?->countryCode ?? 'default') . '.png') }}"
+                        alt="{{ $requestData->combination?->origincountry?->countryCode ?? 'N/A' }}"
+                        class="w-5 h-4 object-cover rounded-sm" />
+                    {{ $requestData->combination?->origincountry?->countryName ?? 'Unknown' }}
+
+                    <span class="mx-1">to</span>
+
+                    <img src="{{ asset('assets/flags/64x48/' . strtolower($requestData->combination?->destinationcountry?->countryCode ?? 'default') . '.png') }}"
+                        alt="{{ $requestData->combination?->destinationcountry?->countryCode ?? 'N/A' }}"
+                        class="w-5 h-4 object-cover rounded-sm" />
+                    {{ $requestData->combination?->destinationcountry?->countryName ?? 'Unknown' }}
+                </span>
+            </td>
+
+            {{-- Visa Details --}}
+            <td class="border-[2px] border-secondary/40 px-4 py-1 text-sm">
+                <span class="block leading-tight">{{ $requestData->visa?->name ?? 'N/A' }}</span>
+                <span class="block font-medium text-xs leading-tight">{{ $requestData->visasubtype?->name ?? 'N/A' }}</span>
+            </td>
+
+            {{-- Nationality --}}
+            <td class="border-[2px] border-secondary/40 px-4 py-1">
+                {{ $requestData->nationality ?? 'N/A' }}
+            </td>
+
+            {{-- Client Info --}}
+            <td class="border-[2px] border-secondary/40 px-4 py-1 text-sm">
+                <span><i class="fa fa-user mr-1"></i>{{ $requestData->full_name ?? 'N/A' }}</span><br>
+                <span class="font-medium text-xs"><i class="fa fa-envelope mr-1"></i>{{ $requestData->email ?? 'N/A' }}</span><br>
+                <span class="font-medium text-xs"><i class="fa fa-phone mr-1"></i>{{ $requestData->phone_number ?? 'N/A' }}</span>
+            </td>
+
+            {{-- Date of Entry --}}
+            <td class="border-[2px] border-secondary/40 px-4 py-1">
+                {{ $requestData->date_of_entry ? \Carbon\Carbon::parse($requestData->date_of_entry)->format('d M Y') : 'N/A' }}
+            </td>
+
+            {{-- Action --}}
+            <td class="border-[2px] border-secondary/40 px-4 py-1">
+                <div class="flex gap-2 items-center">
+                    <a href="{{ route('applications.view', $requestData->id) }}" title="View Application">
+                        <div class="bg-success/10 text-success h-6 w-8 flex justify-center items-center rounded-[3px] hover:bg-success hover:text-white transition ease-in duration-2000">
+                            <i class="fa fa-eye"></i>
+                        </div>
+                    </a>
+
+                    <a href="{{ route('applications.proceed', $requestData->id) }}"
+                       title="Proceed Application"
+                       onclick="return confirm('Are you sure you want to proceed with this application?');">
+                        <div class="bg-warning/10 text-warning h-6 w-8 flex justify-center items-center rounded-[3px] hover:bg-warning hover:text-white transition ease-in duration-2000">
+                            <i class="fa fa-share"></i>
+                        </div>
+                    </a>
+                </div>
+            </td>
+        </tr>
+    @empty
+        <tr>
+            <td colspan="8" class="border-[2px] border-secondary/40 px-4 py-1 text-center">No Record Found</td>
+        </tr>
+    @endforelse
+</table>
+
+
+
             </div>
 {{--        === table section code ends here===--}}
 <script>
